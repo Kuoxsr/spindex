559b299f3d7cec6a4ff3868a0b301104132593ff
Kuoxsr <kuoxsr@gmail.com>
2024-02-06 13:49:46 -0600

    Add 29 new tests to the Defaults class
    Testing the get_sound function. Every possible output option is being
    tested now. If an event default doesn't exist for a particular setting,
    it should use the "all" setting, which is a catch-all for every event
    for which a default hasn't been specified.
    
    Also testing what it looks like to print the values as a string. I'm a
    little concerned that it uses single-quotes rather than double-quotes,
    but according to a Stack Overflow question, using json.dumps should
    correct this. On the other hand, when would I use the string
    representation in the program and show it to a user? Test coverage, I
    guess...
    
    defaults.py has 100% test coverage, but I'm still thinking of adding a
    few more tests.  For example, I should have validation for the incoming
    values.  Right now, you can put in any string or any float, and this
    would not necessarily work for Minecraft.
    
    Overall coverage is at 69%, but I'm considering the 50% goal to only
    apply to sound_pack_indexer.py, which is now at 17% ever since I moved
    the TypedDict objects to a separate file.
    
    Contributes to issue #9

--- objects/defaults.py
+++ objects/defaults.py
@@ -6,8 +6,8 @@ class Defaults:
     def __init__(self, data: dict[str, SoundEventDefaults]):
         self.data: dict[str, SoundEventDefaults] = data
 
-    def __repr__(self):
-        print(f"{self.data}")
+    def __str__(self):
+        return f"{self.data}"
 
     def get_sound_event(self, event_name: str):
 
@@ -28,29 +28,41 @@ class Defaults:
 
     def get_sound(self, event_name, sound_name):
 
-        a: SoundEventDefaults = self.data["all"]
-        d: SoundEventDefaults = self.data[event_name]
+        a: SoundEventDefaults = self.data["all"] if "all" in self.data else SoundEventDefaults()
+        d: SoundEventDefaults = self.data[event_name] if event_name in self.data else SoundEventDefaults()
 
-        default_volume = d["volume"] if d["volume"] else a["volume"] if a["volume"] else None
-        default_pitch = d["pitch"] if d["pitch"] else a["pitch"] if a["pitch"] else None
-        default_weight = d["weight"] if d["weight"] else a["weight"] if a["weight"] else None
-        default_stream = d["stream"] if d["stream"] else a["stream"] if a["stream"] else None
+        default_volume = d["volume"] if "volume" in d else a["volume"] if "volume" in a else None
+        default_weight = d["weight"] if "weight" in d else a["weight"] if "weight" in a else None
+        default_pitch = d["pitch"] if "pitch" in d else a["pitch"] if "pitch" in a else None
+        default_stream = d["stream"] if "stream" in d else a["stream"] if "stream" in a else None
         default_attenuation = \
-            d["attenuation_distance"] if \
-            d["attenuation_distance"] else \
-            a["attenuation_distance"] if \
-            a["attenuation_distance"] else None
-        default_preload = d["preload"] if d["preload"] else a["preload"] if a["preload"] else None
-        default_type = d["type"] if d["type"] else a["type"] if a["type"] else None
+            d["attenuation_distance"] if "attenuation_distance" in d else \
+            a["attenuation_distance"] if "attenuation_distance" in a else None
+        default_preload = d["preload"] if "preload" in d else a["preload"] if "preload" in a else None
+        default_type = d["type"] if "type" in d else a["type"] if "type" in a else None
 
         sound: Sound = Sound(name=sound_name)
-        sound.volume = default_volume
-        sound.pitch = default_pitch
-        sound.weight = default_weight
-        sound.stream = default_stream
-        sound.attenuation_distance = default_attenuation
-        sound.preload = default_preload
-        sound.type = default_type
+
+        if default_volume is not None:
+            sound["volume"] = default_volume
+
+        if default_weight is not None:
+            sound["weight"] = default_weight
+
+        if default_pitch is not None:
+            sound["pitch"] = default_pitch
+
+        if default_stream is not None:
+            sound["stream"] = default_stream
+
+        if default_attenuation is not None:
+            sound["attenuation_distance"] = default_attenuation
+
+        if default_preload is not None:
+            sound["preload"] = default_preload
+
+        if default_type is not None:
+            sound["type"] = default_type
 
         return sound
 
--- tests/test_defaults.py
+++ tests/test_defaults.py
@@ -31,6 +31,8 @@ def test_get_sound_event_should_use_all_when_no_event_replace_exists():
     result = defaults.get_sound_event("test.event")
     assert result["replace"] is True
 
+# ------------------------------------------------------------------------
+
 
 def test_get_sound_event_should_return_empty_list_for_sounds():
 
@@ -38,6 +40,8 @@ def test_get_sound_event_should_return_empty_list_for_sounds():
 
     result = defaults.get_sound_event("test.event")
     assert result["sounds"] == []
+
+# ------------------------------------------------------------------------
 
 
 def test_get_sound_event_should_use_event_subtitle():
@@ -65,6 +69,294 @@ def test_get_sound_event_should_not_honor_subtitle_defaulted_under_all():
     result = defaults.get_sound_event("test.event")
     assert result["subtitle"] == "subtitles.test.event"
 
+# ------------------------------------------------------------------------
 
-def test_get_sound():
-    assert True
+
+def test_get_sound_should_use_event_volume():
+
+    event_defaults = SoundEventDefaults(volume=0.1)
+    defaults = Defaults({"test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["volume"] == 0.1
+
+
+def test_get_sound_should_use_event_volume_instead_of_all():
+
+    all_defaults = SoundEventDefaults(volume=0.9)
+    event_defaults = SoundEventDefaults(volume=0.1)
+    defaults = Defaults({"all": all_defaults, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["volume"] == 0.1
+
+
+def test_get_sound_should_use_all_when_no_event_volume_exists():
+
+    all_defaults = SoundEventDefaults(volume=0.9)
+    event_defaults = SoundEventDefaults(weight=8)
+    defaults = Defaults({"all": all_defaults, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["volume"] == 0.9
+
+
+def test_get_sound_should_not_include_volume_when_no_default_specified():
+
+    all_defaults = SoundEventDefaults()
+    defaults = Defaults({"all": all_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert "volume" not in result
+
+# ------------------------------------------------------------------------
+
+
+def test_get_sound_should_use_event_weight():
+
+    event_defaults = SoundEventDefaults(weight=8)
+    defaults = Defaults({"test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["weight"] == 8
+
+
+def test_get_sound_should_use_event_weight_instead_of_all():
+
+    all_details = SoundEventDefaults(weight=1)
+    event_defaults = SoundEventDefaults(weight=8)
+    defaults = Defaults({"all": all_details, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["weight"] == 8
+
+
+def test_get_sound_should_use_all_when_no_event_weight_exists():
+
+    all_details = SoundEventDefaults(weight=1)
+    event_defaults = SoundEventDefaults(volume=0.1)
+    defaults = Defaults({"all": all_details, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["weight"] == 1
+
+
+def test_get_sound_should_not_include_weight_when_no_default_specified():
+
+    all_defaults = SoundEventDefaults()
+    defaults = Defaults({"all": all_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert "weight" not in result
+
+# ------------------------------------------------------------------------
+
+
+def test_get_sound_should_use_event_pitch():
+
+    event_defaults = SoundEventDefaults(pitch=1.3)
+    defaults = Defaults({"test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["pitch"] == 1.3
+
+
+def test_get_sound_should_use_event_pitch_instead_of_all():
+
+    all_details = SoundEventDefaults(pitch=5.4)
+    event_defaults = SoundEventDefaults(pitch=1.3)
+    defaults = Defaults({"all": all_details, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["pitch"] == 1.3
+
+
+def test_get_sound_should_use_all_when_no_event_pitch_exists():
+
+    all_details = SoundEventDefaults(pitch=5.4)
+    event_defaults = SoundEventDefaults(volume=1.3)
+    defaults = Defaults({"all": all_details, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["pitch"] == 5.4
+
+
+def test_get_sound_should_not_include_pitch_when_no_default_specified():
+
+    all_defaults = SoundEventDefaults()
+    defaults = Defaults({"all": all_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert "pitch" not in result
+
+# ------------------------------------------------------------------------
+
+
+def test_get_sound_should_use_event_stream():
+
+    event_defaults = SoundEventDefaults(stream=True)
+    defaults = Defaults({"test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["stream"] is True
+
+
+def test_get_sound_should_use_event_stream_instead_of_all():
+
+    all_details = SoundEventDefaults(stream=False)
+    event_defaults = SoundEventDefaults(stream=True)
+    defaults = Defaults({"all": all_details, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["stream"] is True
+
+
+def test_get_sound_should_use_all_when_no_event_stream_exists():
+
+    all_details = SoundEventDefaults(stream=False)
+    event_defaults = SoundEventDefaults(volume=0.1)
+    defaults = Defaults({"all": all_details, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["stream"] is False
+
+
+def test_get_sound_should_not_include_stream_when_no_default_specified():
+
+    all_defaults = SoundEventDefaults()
+    defaults = Defaults({"all": all_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert "stream" not in result
+
+# ------------------------------------------------------------------------
+
+
+def test_get_sound_should_use_event_attenuation():
+
+    event_defaults = SoundEventDefaults(attenuation_distance=3)
+    defaults = Defaults({"test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["attenuation_distance"] == 3
+
+
+def test_get_sound_should_use_event_attenuation_instead_of_all():
+
+    all_details = SoundEventDefaults(attenuation_distance=42)
+    event_defaults = SoundEventDefaults(attenuation_distance=3)
+    defaults = Defaults({"all": all_details, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["attenuation_distance"] == 3
+
+
+def test_get_sound_should_use_all_when_no_event_attenuation_exists():
+
+    all_details = SoundEventDefaults(attenuation_distance=42)
+    event_defaults = SoundEventDefaults(volume=0.1)
+    defaults = Defaults({"all": all_details, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["attenuation_distance"] == 42
+
+
+def test_get_sound_should_not_include_attenuation_when_no_default_specified():
+
+    all_defaults = SoundEventDefaults()
+    defaults = Defaults({"all": all_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert "attenuation_distance" not in result
+
+# ------------------------------------------------------------------------
+
+
+def test_get_sound_should_use_event_preload():
+
+    event_defaults = SoundEventDefaults(preload=True)
+    defaults = Defaults({"test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["preload"] is True
+
+
+def test_get_sound_should_use_event_preload_instead_of_all():
+
+    all_details = SoundEventDefaults(preload=False)
+    event_defaults = SoundEventDefaults(preload=True)
+    defaults = Defaults({"all": all_details, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["preload"] is True
+
+
+def test_get_sound_should_use_all_when_no_event_preload_exists():
+
+    all_details = SoundEventDefaults(preload=False)
+    event_defaults = SoundEventDefaults(volume=0.1)
+    defaults = Defaults({"all": all_details, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["preload"] is False
+
+
+def test_get_sound_should_not_include_preload_when_no_default_specified():
+
+    all_defaults = SoundEventDefaults()
+    defaults = Defaults({"all": all_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert "preload" not in result
+
+# ------------------------------------------------------------------------
+
+
+def test_get_sound_should_use_event_type():
+
+    event_defaults = SoundEventDefaults(type="event type")
+    defaults = Defaults({"test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["type"] == "event type"
+
+
+def test_get_sound_should_use_event_type_instead_of_all():
+
+    all_details = SoundEventDefaults(type="all type")
+    event_defaults = SoundEventDefaults(type="event type")
+    defaults = Defaults({"all": all_details, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["type"] == "event type"
+
+
+def test_get_sound_should_use_all_when_no_event_type_exists():
+
+    all_details = SoundEventDefaults(type="all type")
+    event_defaults = SoundEventDefaults(volume=0.1)
+    defaults = Defaults({"all": all_details, "test.event": event_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert result["type"] == "all type"
+
+
+def test_get_sound_should_not_include_type_when_no_default_specified():
+
+    all_defaults = SoundEventDefaults()
+    defaults = Defaults({"all": all_defaults})
+
+    result = defaults.get_sound("test.event", "test_sound_name")
+    assert "type" not in result
+
+# ------------------------------------------------------------------------
+
+
+def test_get_sound_should_print_correctly():
+
+    all_details = SoundEventDefaults(type="all type")
+    event_defaults = SoundEventDefaults(volume=0.1)
+    defaults = Defaults({"all": all_details, "test.event": event_defaults})
+
+    result: str = f"{defaults}"
+    assert result == "{'all': {'type': 'all type'}, 'test.event': {'volume': 0.1}}"

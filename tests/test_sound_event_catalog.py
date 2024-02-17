e161f02a87e5396c24d1e1ab8877e24fd2121100
Kuoxsr <kuoxsr@gmail.com>
2024-02-17 01:51:40 -0600

    Add SoundEventCatalog and tests to verify its one method
    The data for SoundEventCatalog is built using a script that crawls
    through the minecraft vanilla sounds.json looking for sound event
    names. It pulls the first segment of each name (the word prior to the
    first dot), and makes that a key to a dictionary. The value(s) pointed
    to by that key are a list of sound event names that all begin with that
    same first segment.
    
    The technique used in get_sound_event_name is to look for the first
    occurrence of the dictionary key in an incoming path, and then build a
    sound event name from the path starting at the index of the match.
    
    If the event name constructed from the path doesn't match any of the
    names in the catalog, a SoundEventValueError is raised. If it walks
    through the entire path and doesn't match the dictionary key at all,
    another SoundEventValueError is raised with a different message.
    
    This should replace get_sound_event_name from the main application, as
    it has the ability to verify a constructed name against a list of names
    that Minecraft actually expects.
    
    Implements issue #17

---
+++ tests/test_sound_event_catalog.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from sound_event_catalog import SoundEventCatalog, SoundEventValueError
+
+import pytest
+
+
+def test_sound_event_catalog_get_sound_event_name_should_return_correct_name():
+
+    path: Path = Path("/good/path/weird/entity/villager/ambient/ogg_file_name")
+
+    catalog = SoundEventCatalog()
+    result = catalog.get_sound_event_name(path)
+
+    assert result == "entity.villager.ambient"
+
+
+def test_sound_event_catalog_get_sound_event_name_should_raise_error_when_constructed_path_incorrect():
+
+    path: Path = Path("/path/weird/entity/villager/whatever/ogg_file_name")
+
+    catalog = SoundEventCatalog()
+
+    with pytest.raises(SoundEventValueError) as result:
+        catalog.get_sound_event_name(path)
+
+    assert str(result.value) == "The constructed event name (entity.villager.whatever) was not found in catalog"
+
+
+def test_sound_event_catalog_get_sound_event_name_should_raise_error_when_event_name_cannot_be_constructed():
+
+    path: Path = Path("/not/really/a/sound/event_name/ogg_file_name")
+
+    catalog = SoundEventCatalog()
+
+    with pytest.raises(SoundEventValueError) as result:
+        catalog.get_sound_event_name(path)
+
+    assert str(result.value) == f"Could not build a sound event from this path: {path}"
